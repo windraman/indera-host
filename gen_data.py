640299b9f5631c590df7937bc7e8ebba044e2097
Wahyu Indraman <windraman@yahoo.com>
2020-08-07 14:19:49 +0800

    Update gen_data.py

--- gen_data.py
+++ gen_data.py
@@ -15,11 +15,11 @@ conn.commit()
 
 grup = db.execute('''SELECT * from grups ORDER BY id DESC LIMIT 1''').fetchone()
 
-gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,tipe_sensor_id,grups_id) VALUES ("LED 16","'''+ str(grup['slug']) + '''_led_16","16","2","''' + str(grup['id']) + '''")''')
+gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,pin2,tipe_sensor_id,grups_id) VALUES ("LED 16","'''+ str(grup['slug']) + '''_led_16","16","2","2","''' + str(grup['id']) + '''")''')
 conn.commit()
-gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,tipe_sensor_id,grups_id) VALUES ("LED 20","'''+ str(grup['slug']) + '''_led_20","20","2","''' + str(grup['id']) + '''")''')
+gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,pin2,tipe_sensor_id,grups_id) VALUES ("LED 20","'''+ str(grup['slug']) + '''_led_20","20","2","2","''' + str(grup['id']) + '''")''')
 conn.commit()
-gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,tipe_sensor_id,grups_id) VALUES ("LED 21","'''+ str(grup['slug']) + '''_led_21","21","2","''' + str(grup['id']) + '''")''')
+gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,pin2,tipe_sensor_id,grups_id) VALUES ("LED 21","'''+ str(grup['slug']) + '''_led_21","21","2","2","''' + str(grup['id']) + '''")''')
 conn.commit()
 
 gengrup = db.execute('''INSERT INTO grups(name,slug,moduls_id) VALUES ("Button grup","''' + str(rows['slug']) + '''_button_grup","''' + str(rows['id']) + '''")''')
@@ -27,9 +27,9 @@ conn.commit()
 
 grup = db.execute('''SELECT * from grups ORDER BY id DESC LIMIT 1''').fetchone()
 conn.commit()
-gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,tipe_sensor_id,grups_id) VALUES ("Button 5","'''+ str(grup['slug']) + '''_button_5","5","1","''' + str(grup['id']) + '''")''')
+gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,pin2,tipe_sensor_id,grups_id) VALUES ("Button 5","'''+ str(grup['slug']) + '''_button_5","5","2","1","''' + str(grup['id']) + '''")''')
 conn.commit()
-gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,tipe_sensor_id,grups_id) VALUES ("Button 6","'''+ str(grup['slug']) + '''_button_6","6","1","''' + str(grup['id']) + '''")''')
+gensensor = db.execute('''INSERT INTO sensors(name,slug,pin,pin2,tipe_sensor_id,grups_id) VALUES ("Button 6","'''+ str(grup['slug']) + '''_button_6","6","2","1","''' + str(grup['id']) + '''")''')
 conn.commit()
 
 conn.close()
