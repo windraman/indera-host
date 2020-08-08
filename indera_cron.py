00e5e902e43003bad080a9fc31946692b4a0fba5
Wahyu Indraman <windraman@yahoo.com>
2020-08-08 12:25:13 +0800

    Update indera_cron.py

--- indera_cron.py
+++ indera_cron.py
@@ -3,16 +3,39 @@ import os
 import sys
 app_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 
-with CronTab(user=sys.argv[1]) as cron:
-    job = cron.new(command='python3 '+app_path+'moderator.py')
-    job.every_reboot()
-    job = cron.new(command='python3 '+app_path+'send_ip.py')
-    job.every_reboot()
-    job = cron.new(command='python3 '+app_path+'moderator.py')
-    job.minute.every(1)
-    job = cron.new(command='python3 '+app_path+'send_ip.py')
-    job.minute.every(3)
-    job = cron.new(command='python3 '+app_path+'report_terjadwal.py')
-    job.every(20).hours()
+def notExist(cron,line):
+    if any(line in str(job) for job in cron):
+        print("job exist")
+        return False
+    else:
+        return True
+
+#with CronTab(user=sys.argv[1]) as cron:
+with CronTab(user="pi") as cron:
+    if(notExist(cron,'@reboot python3 '+app_path+'moderator.py')):
+        job = cron.new(command='python3 '+app_path+'moderator.py')
+        job.every_reboot()  
+        print("created")      
+    
+    if(notExist(cron,'@reboot python3 '+app_path+'send_ip.py')):
+        job = cron.new(command='python3 '+app_path+'send_ip.py')
+        job.every_reboot()  
+        print("created")    
+        
+    if notExist(cron,'* * * * * python3 '+app_path+'moderator.py'):
+        job = cron.new(command='python3 '+app_path+'moderator.py')
+        job.minute.every(1)
+        print('created')    
+            
+    if notExist(cron,'*/3 * * * * python3 '+app_path+'send_ip.py'):
+        job = cron.new(command='python3 '+app_path+'send_ip.py')
+        job.minute.every(3)
+        print('created')
+        
+    if notExist(cron,'* 7 * * * python3 '+app_path+'report_terjadwal.py'):
+        job = cron.new(command='python3 '+app_path+'report_terjadwal.py')
+        job.hours.on(7)
+        print('created')
+        
     for job in cron:
         print(job)
